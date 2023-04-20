bfbe29cd8b181113d87f2ec4d85858a304c08a75
kunul942 <jhonatanlee942@gmail.com>
2023-04-19 19:18:37 -0600

    Fix

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -22,7 +22,8 @@ const Computers = ({ isMobile }) => {
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        // position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        position={isMobile ? [0, -2, -1.1] : [0, -2.25, -0.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -55,7 +56,6 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameloop='demand'
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
